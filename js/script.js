a317d8f820604ab361cb903897d0f88129e70f29
João Gabriel <jgmoura.dev@gmail.com>
2022-11-03 09:27:41 -0300

    navegacao prev e nev finalizada

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-import Slide from "./slide.js";
-const slide = new Slide(".slide", ".slide-wrapper");
+import { SlideNav } from "./slide.js";
+const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 
 slide.changeSlide(2);
 slide.activeNextSlide();
+slide.addArrow(".prev", ".next");
+
+console.log(slide);
